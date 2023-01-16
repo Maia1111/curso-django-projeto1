90a6d391af63588ebfb3bff4e8968bab41b688e1
Rogério Maia <rogerioreservadosinop@gmail.com>
2023-01-16 09:57:35 -0400

    Criado a model Recipe e Category mais não criado a migrate

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    preparation_steps = models.TextField()
+    prepation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+    is_publishe = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
