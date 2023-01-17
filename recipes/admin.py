fcba5ea30622c85d77210dc863028e7077fd1e33
Rogério Maia <rogerioreservadosinop@gmail.com>
2023-01-17 09:05:06 -0400

    Contruida as models Recipe e Category, configurado  superusuário

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+
+# Forma 1 de registrar no admin
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+# Forma 1 de registrar no admin
+admin.site.register(Category, CategoryAdmin)
+
+
+# Forma 2 de registra no admin usando decorate
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -13,6 +16,7 @@ class Recipe(models.Model):
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     prepation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -22,3 +26,6 @@ class Recipe(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
